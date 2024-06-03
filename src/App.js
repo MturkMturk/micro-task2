d18f728f3d05dc43ffe4f0a8a9a6bc5a9a477de1
AVM6LXK_thdgit <ALBIN_V_MARIA_AROCKIAM@homedepot.com>
2024-06-03 19:20:24 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import React, { Component } from 'react';
-import Iframe from './iframe.js';
+import React from 'react';
+import One from './One';
 
-class App extends Component {
-  state = {
-    password: '',
-    isAuthenticated: false,
-    enteredPassword: '',
-  };
-
-  handleChange = (event) => {
-    this.setState({ enteredPassword: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const hardcodedPassword = 'hardcoded'; // Replace with your hardcoded password
-    if (this.state.enteredPassword === hardcodedPassword) {
-      this.setState({ isAuthenticated: true });
-    } else {
-      alert('Incorrect password');
-    }
-  };
-
-  render() {
-    return (
-      <div className="App">
-        {!this.state.isAuthenticated ? (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Enter Password:
-              <input
-                type="password"
-                value={this.state.enteredPassword}
-                onChange={this.handleChange}
-              />
-            </label>
-            <button type="submit">Submit</button>
-          </form>
-        ) : (
-          <div className="iframe-container">
-            <Iframe
-              url="https://cdn.jwplayer.com/players/k4g6nHV1-PzqYXf16.html"
-              width="100%"
-              height="100%"
-              id="myId"
-              className="myClassname"
-              display="initial"
-              position="relative"
-              allowFullScreen
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/one' element={<One />} />
+    </Routes>
+  );
 }
 
 export default App;
