64367ae4ff07b58b7d5363e2d39e7abd9bb4a8ea
AVM6LXK_thdgit <ALBIN_V_MARIA_AROCKIAM@homedepot.com>
2024-06-03 19:32:21 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import One from './One';
+import Two from './Two';
+import Three from './Three';
+import Four from './Four';
+import Five from './Five';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/one' element={<One />} />
+      <Route path='/two' element={<Two />} />
+      <Route path='/three' element={<Three />} />
+      <Route path='/four' element={<Four />} />
+      <Route path='/five' element={<Five />} />
     </Routes>
   );
 }
---
+++ src/Two.js
@@ -0,0 +1,60 @@
+import './App.css';
+import React, { Component } from 'react';
+import Iframe from './iframe.js';
+
+class Two extends Component {
+  state = {
+    password: '',
+    isAuthenticated: false,
+    enteredPassword: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({ enteredPassword: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const hardcodedPassword = 'hardcoded'; // Replace with your hardcoded password
+    if (this.state.enteredPassword === hardcodedPassword) {
+      this.setState({ isAuthenticated: true });
+    } else {
+      alert('Incorrect password');
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {!this.state.isAuthenticated ? (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Enter Password:
+              <input
+                type="password"
+                value={this.state.enteredPassword}
+                onChange={this.handleChange}
+              />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        ) : (
+          <div className="iframe-container">
+            <Iframe
+              url="https://cdn.jwplayer.com/players/HXtdRpnG-PzqYXf16.html"
+              width="100%"
+              height="100%"
+              id="myId"
+              className="myClassname"
+              display="initial"
+              position="relative"
+              allowFullScreen
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Two;
