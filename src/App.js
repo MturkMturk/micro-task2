719fc795e6c77cefc3140764b0cb6c540eec23e6
AVM6LXK_thdgit <ALBIN_V_MARIA_AROCKIAM@homedepot.com>
2024-06-03 18:05:23 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import Iframe from './iframe.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    password: '',
+    isAuthenticated: false,
+    enteredPassword: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({ enteredPassword: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const hardcodedPassword = 'hardcoded'; // Replace with your hardcoded password
+    if (this.state.enteredPassword === hardcodedPassword) {
+      this.setState({ isAuthenticated: true });
+    } else {
+      alert('Incorrect password');
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {!this.state.isAuthenticated ? (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Enter Password:
+              <input
+                type="password"
+                value={this.state.enteredPassword}
+                onChange={this.handleChange}
+              />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        ) : (
+          <Iframe
+            url="https://cdn.jwplayer.com/players/HXtdRpnG-PzqYXf16.html"
+            width="450px"
+            height="450px"
+            id="myId"
+            className="myClassname"
+            display="initial"
+            position="relative"
+            allowFullScreen
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Correct import for React 18
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
+// Create a root
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the application
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
