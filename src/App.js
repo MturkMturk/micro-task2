648319741bcbd9f70f14280677aefbdd096f2bb3
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2024-06-03 19:05:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
         ) : (
           <div className="iframe-container">
             <Iframe
-              url="https://cdn.jwplayer.com/players/HXtdRpnG-PzqYXf16.html"
+              url="https://cdn.jwplayer.com/players/k4g6nHV1-PzqYXf16.html"
               width="100%"
               height="100%"
               id="myId"
