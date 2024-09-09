88bbccb6273a91b22fd0a7217a62fd7728ea8b19
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2024-09-09 10:25:25 -0400

    Update Seven.js

--- src/Seven.js
+++ src/Seven.js
@@ -43,7 +43,7 @@ class Seven extends Component {
         ) : (
           <div className="iframe-container">
             <Iframe
-              url="https://cdn.jwplayer.com/players/QWjYcPD9-PzqYXf16.html"
+              url="https://cdn.jwplayer.com/players/nQOF6f7m-PzqYXf16.html"
               width="100%"
               height="100%"
               id="myId"
